15ff7dcfa3f1f26daf80964d686789e85b2db9b9
Thomas Kasbarian2 <thomas.kasbarian@gmail.com>
2024-05-28 09:04:47 +0200

    sed is for losers

--- cpp01/ex04/fileUtils.cpp
+++ cpp01/ex04/fileUtils.cpp
@@ -1,20 +1,21 @@
 #include "fileUtils.hpp"
-# include <iostream>
-# include <string>
+#include <iostream>
+#include <string>
+#include <sstream>
+
 
 int	openInFile(std::ifstream& fStream, const std::string& fPath) {
 	fStream.open(fPath.c_str());
-	if (fStream.fail()) {
+	if (!fStream.is_open()) {
 		std::cerr << "Error opening file: " << fPath << std::endl;
 		return (ERROR);
 	}
 	return (SUCCESS);
 }
 
-// Todo: merge these 2 functions??
 int	openOutFile(std::ofstream& fStream, const std::string& fPath) {
 	fStream.open(fPath.c_str());
-	if (fStream.fail()) {
+	if (!fStream.is_open()) {
 		std::cerr << "Error opening file: " << fPath << std::endl;
 		return (ERROR);
 	}
@@ -22,28 +23,30 @@ int	openOutFile(std::ofstream& fStream, const std::string& fPath) {
 }
 
 std::string	readFileContent(std::ifstream& inStream) {
-	std::string	line;
-	std::string	content = "";
+	std::string			line;
+	std::string			content;
+	std::stringstream	buffer;
 
-	content = "";
-	while (std::getline(inStream, line))
-		content += line;
+	buffer << inStream.rdbuf();
+	content = buffer.str();
 	return (content);
 }
 
 std::string	stringReplace(const std::string& sourceStr, const char* searchCstr, const char* replaceCstr) {
 	std::string				search(searchCstr);
 	std::string				replace(replaceCstr);
-	std::string				newStr = "";
+	std::string				newStr;
 	std::string::size_type	sourcePos = 0;
 	std::string::size_type	searchHit = 0;
 
+	if (search.length() < 1)
+		return (sourceStr);
 	while ((searchHit = sourceStr.find(search, sourcePos)) != std::string::npos) {
-		std::cout << "searchhit: " << searchHit << std::endl;
-		std::cout << "substr: " << sourceStr.substr(sourcePos, searchHit - sourcePos) << std::endl;
 		newStr.append(sourceStr, sourcePos, searchHit - sourcePos);
 		newStr.append(replace);
-		sourcePos += search.length();
+		sourcePos = searchHit + search.length();
 	}
+	if (sourcePos < sourceStr.length())
+		newStr.append(sourceStr, sourcePos, sourceStr.length() - sourcePos);
 	return (newStr);
 }
--- cpp01/ex04/fileUtils.hpp
+++ cpp01/ex04/fileUtils.hpp
@@ -2,7 +2,7 @@
 # define FILEUTILS_HPP
 
 # include <string>
-#include <fstream>
+# include <fstream>
 
 # ifndef SUCCESS
 #  define SUCCESS 0
@@ -11,8 +11,9 @@
 #  define ERROR 1
 # endif
 
-int	openInFile(std::ifstream& fStream, const std::string& fPath);
-int	openOutFile(std::ofstream& fStream, const std::string& fPath);
+
+int			openInFile(std::ifstream& fStream, const std::string& fPath);
+int			openOutFile(std::ofstream& fStream, const std::string& fPath);
 std::string	readFileContent(std::ifstream& inStream);
 std::string	stringReplace(const std::string& src, const char* searchStr, const char* replaceStr);
 
